1da03aef883c077643c5e31b4a6c6ea2cac99f2a
Muhammad Ahmad <prince1ahmed111@gmail.com>
2024-11-01 17:26:13 +0500

    page.js import change

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
-import HeroSection from "@/components/HeroSection";
-import Navbar from "@/components/NavBar";
-import PremiumDoctorsSection from "@/components/PremiumDoctorsSection";
-import SecondSection from "@/components/SecondSection";
-import ShortBackgroundSection from "@/components/ShortBackgroundSection";
-import ShortBgSectionTwo from "@/components/ShortBgSectionTwo";
-import ThirdHeroSection from "@/components/ThirdHeroSection";
 import Image from "next/image";
-
+import Navbar from "@/components/NavBar";
+import SecondSection from "@/components/SecondSection";
+import ThirdHeroSection from "@/components/ThirdHeroSection";
+import ShortBackgroundSection from "@/components/ShortBackgroundSection";
+import PremiumDoctorsSection from "@/components/PremiumDoctorsSection";
+import ShortBgSectionTwo from "@/components/ShortBgSectionTwo";
 export default function Home() {
   return (
     <div className="">

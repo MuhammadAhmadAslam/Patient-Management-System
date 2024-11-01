fef8df8cb2649e30c86bebdd6661831c27584928
Muhammad Ahmad <prince1ahmed111@gmail.com>
2024-11-01 17:13:27 +0500

    components c small

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import HeroSection from "@/Components/HeroSection";
-import Navbar from "@/Components/NavBar";
-import PremiumDoctorsSection from "@/Components/PremiumDoctorsSection";
-import SecondSection from "@/Components/SecondSection";
-import ShortBackgroundSection from "@/Components/ShortBackgroundSection";
-import ShortBgSectionTwo from "@/Components/ShortBgSectionTwo";
-import ThirdHeroSection from "@/Components/ThirdHeroSection";
+import HeroSection from "@/components/HeroSection";
+import Navbar from "@/components/NavBar";
+import PremiumDoctorsSection from "@/components/PremiumDoctorsSection";
+import SecondSection from "@/components/SecondSection";
+import ShortBackgroundSection from "@/components/ShortBackgroundSection";
+import ShortBgSectionTwo from "@/components/ShortBgSectionTwo";
+import ThirdHeroSection from "@/components/ThirdHeroSection";
 import Image from "next/image";
 
 export default function Home() {
